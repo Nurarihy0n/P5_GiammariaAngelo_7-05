3ad2ed5d5481f8d28b6efcfeddd63cd6cc7c132d
nurarihyon <giammaria.angelo@gmail.com>
2021-02-25 18:28:24 +0900

    XML Card HomePage

--- Backend/view/script.js
+++ Backend/view/script.js
@@ -53,73 +53,73 @@ var request = new XMLHttpRequest;
 
 request.onreadystatechange = function () {
     if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-        var response = JSON.parse(this.responseText);
-        console.log(response);
+        var camera = JSON.parse(this.responseText);
+        console.log(camera);
+        
+
+        for (i = 0; i < camera.length; i++) {
+
+            // creation element carte 
+            var section = document.getElementById('carte-test');
+            var divContainerCard = document.getElementById('divContainer');
+            var divRow = document.getElementById('divRow');
+            var divCol = document.createElement('div');
+            divCol.className = 'col-12 col-md-4 my-2';
+
+            // card
+            var divCard = document.createElement('div');
+            divCard.className = 'card card-cascade card-ecommerce border-black shadow';
+
+            //card image
+            var divCardImg = document.createElement('div');
+            divCardImg.className = 'view view-cascade overlay';
+            var image = document.createElement('img');
+            image.className = 'card-img-top'
+            var a = document.createElement('a');
+            var divMask = document.createElement('div');
+            divMask.className = 'mask rgba-white-slight';
+
+            //card content
+            var divCardContent = document.createElement('div');
+            divCardContent.className = 'card-body card-body-cascade text-center';
+            var carteTitle = document.createElement('h4');
+            carteTitle.className = 'card-title';
+            var paragraphe = document.createElement('p');
+            paragraphe.className = 'card-text';
+
+            //card footer
+            var divFooter = document.createElement('div');
+            divFooter.className = 'card-footer px-1';
+            var spanFooter = document.createElement('span');
+            spanFooter.className = 'float-letf float-weight-bold';
+            var price = document.createElement('strong');
+
+            // init appendChild Card
+            divRow.appendChild(divCol);
+
+
+            divCol.appendChild(divCard);
+            divCard.appendChild(divCardImg);
+            divCardImg.appendChild(image);
+            image.appendChild(a);
+            a.appendChild(divMask);
+
+            divCardImg.appendChild(divCardContent);
+            divCardContent.appendChild(carteTitle);
+            divCardContent.appendChild(paragraphe);
+
+            divCard.appendChild(divFooter);
+            divFooter.appendChild(spanFooter);
+            spanFooter.appendChild(price);
+
+
+            image.src = camera[i].imageUrl;
+            carteTitle.textContent = camera[i].name;
+            paragraphe.textContent = camera[i].description;
+            price.textContent = camera[i].price + '$';
+        }
     }
 }
-
-for (i = 0; i < getCamera.length; i++) {
-    console.log(getCamera[i].name);
-    console.log(getCamera[i].description);
-
-    // creation element carte 
-    var section = document.getElementById('carte-test');
-    var divContainerCard = document.getElementById('divContainer');
-    var divRow = document.getElementById('divRow');
-    var divCol = document.createElement('div');
-    divCol.className = 'col-12 col-md-4';
-
-    // card
-    var divCard = document.createElement('div');
-    divCard.className = 'card card-cascade card-ecommerce border-black shadow';
-
-    //card image
-    var divCardImg = document.createElement('div');
-    divCardImg.className = 'view view-cascade overlay';
-    var image = document.createElement('img');
-    image.className = 'card-img-top'
-    var a = document.createElement('a');
-    var divMask = document.createElement('div');
-    divMask.className = 'mask rgba-white-slight';
-
-    //card content
-    var divCardContent = document.createElement('div');
-    divCardContent.className = 'card-body card-body-cascade text-center';
-    var carteTitle = document.createElement('h4');
-    carteTitle.className = 'card-title';
-    var paragraphe = document.createElement('p');
-    paragraphe.className = 'card-text';
-
-    //card footer
-    var divFooter = document.createElement('div');
-    divFooter.className = 'card-footer px-1';
-    var spanFooter = document.createElement('span');
-    spanFooter.className = 'float-letf float-weight-bold';
-    var price = document.createElement('strong');
-
-    // init appendChild Card
-    divRow.appendChild(divCol);
-
-
-    divCol.appendChild(divCard);
-    divCard.appendChild(divCardImg);
-    divCardImg.appendChild(image);
-    image.appendChild(a);
-    a.appendChild(divMask);
-
-    divCardImg.appendChild(divCardContent);
-    divCardContent.appendChild(carteTitle);
-    divCardContent.appendChild(paragraphe);
-
-    divCard.appendChild(divFooter);
-    divFooter.appendChild(spanFooter);
-    spanFooter.appendChild(price);
-
-
-    image.src = camera[i].imageUrl;
-    carteTitle.textContent = camera[i].name;
-    paragraphe.textContent = camera[i].description;
-    price.textContent = camera[i].price + '$';
 
     request.open('GET', 'http://localhost:3000/api/cameras');
     request.send();
