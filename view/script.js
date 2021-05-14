0b7704a09238b1e32403cab64d377983ac75da1a
nurarihyon <giammaria.angelo@gmail.com>
2021-05-14 19:31:12 +0900

    finalisation page details avant ajout prix

--- view/script.js
+++ view/script.js
@@ -1,7 +1,7 @@
 
 //Validation des champs du formulaire et envoi
 
-let formCart = function () {
+function formCart() {
     document.forms['enregistrement'].addEventListener("submit", function (e) {
 
         let erreur;
@@ -22,9 +22,9 @@ let formCart = function () {
         }
     });
 }
-// -------------------------------------------------------------------------------------------------
+// -------------------------PAGE D'ACCEUIL------------------------------------------------------------------------
 
-let indexProduct = function () {
+function indexProduct() {
 
     // Requete fetch (API)
     const getCamera = async function () {
@@ -39,6 +39,7 @@ let indexProduct = function () {
                 return Error(error);
             });
     }
+
 
     const afficherCamera = async function (produits) {
         produits.forEach(function (camera) {
@@ -123,12 +124,13 @@ let indexProduct = function () {
     }
 
     getCamera()
-
 }
 
-let productDetailsPage = function (json) {
+//----------------------PAGE DU CHOIX DE LA LENTILLE ET D'AJOUT AU PANIER---------------------
 
-    //1. Recuperer l'id (possibilite meme technique pour lentille dans panier avec urlParams.get)
+function productDetailsPage(json) {
+
+    //1. Recuperer l'id
     let urlParams = new URLSearchParams(window.location.search);
     let cameraId = urlParams.get("camera");
     let cameraURL = 'http://localhost:3000/api/cameras/' + cameraId;
@@ -142,72 +144,77 @@ let productDetailsPage = function (json) {
             }
         })
         .then(response => response.json())
-        .then(json => afficherDetailsCard(json))
+        .then(json => productDetailsCard(json))
+        .catch(error => console.log({ error }));
+}
+
+//3. Mettre en place la card 
+
+function productDetailsCard(camera) {
+
+    document.getElementById('imageDiv').src = camera.imageUrl;
+    document.getElementById('cardTitle').textContent = camera.name;
+    document.getElementById('pDescription').textContent = camera.description;
+
+
+    let select = document.getElementById('cameraLenses');
+    let options = camera.lenses;
+    console.log(options);
+
+    for (let i=0; i < options.length; i++){
+        let option = options[i];
+        let optionElt = document.createElement('option');
+        optionElt.setAttribute('value', option);
+        optionElt.textContent = option;
+        select.appendChild(optionElt);
+    }
+
+    let detailCard = document.getElementById('detailCard');
+    let rowLower = document.createElement('div');
+    rowLower.className = 'row lower';
+    let divButton = document.createElement('div');
+    divButton.className = 'col text-right align-self-center divButton';
+    let buttonAddCart = document.createElement('button');
+    buttonAddCart.className = 'btn btn-primary';
+    buttonAddCart.setAttribute("onclick", `addToCart("${camera._id}")`);
+    buttonAddCart.textContent = 'Ajouter au panier';
+
+    detailCard.appendChild(rowLower);
+    rowLower.appendChild(divButton);
+    divButton.appendChild(buttonAddCart);
+
+}
+
+function addToCart(cameraId) {
+    let cameraURL = 'http://localhost:3000/api/cameras/' + cameraId;
+    console.log('addToCart');
+    fetch(cameraURL,
+        {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json'
+            }
+        })
+        .then(response => response.json())
+        .then(json => addProductToCard(json))
         .catch(error => console.log({ error }));
 
-    //3. Mettre en place la card 
+}
 
-    function afficherDetailsCard(camera) {
-        let rowDetail = document.getElementById('divRowDetail');
-        let divImg = document.createElement('div');
-        divImg.className = 'col-md-6 text-center align-self-center';
-        let imageDiv = document.createElement('img');
-        imageDiv.className = 'img-fluid';
-        imageDiv.src = camera.imageUrl;
+function addProductToCard(camera) {
+    let panier = localStorage.getItem("panier");
 
-        let divInfo = document.createElement('div');
-        divInfo.className = 'col-md-6 info';
-        let rowTitle = document.createElement('div');
-        rowTitle.className = 'row title rowTitle';
-        let col = document.createElement('div');
-        col.className = 'col colDetailCard';
-        let cardName = document.createElement('h2');
-        cardName.textContent = camera.name;
-
-        let pDescription = document.createElement('p');
-        pDescription.className = 'pDescription';
-        pDescription.textContent = camera.description;
-        let aFocus = document.createElement('a');
-        let rowPrice = document.createElement('div');
-        rowPrice.className = 'row price';
-        let label = document.createElement('label');
-        label.className = 'radio';
-        let input = document.createElement('input');
-        input.setAttribute = ('type', 'radio');
-        input.setAttribute = ('name', "focus1");
-        input.setAttribute = ('checked');
-        let divRowFocus = document.createElement('div');
-        divRowFocus.className = 'row';
-        divRowFocus.textContent = camera.lenses;
-
-        let detailCard = document.getElementById('detailCard');
-        let rowLower = document.createElement('div');
-        rowLower.className = 'row lower';
-        let divButton = document.createElement('div');
-        divButton.className = 'col text-right align-self-center divButton';
-        let buttonAddCart = document.createElement('button');
-        buttonAddCart.className = 'btnAddCart';
-        buttonAddCart.textContent = 'Ajouter au panier';
-
-
-        rowDetail.appendChild(divImg);
-        divImg.appendChild(imageDiv);
-        
-        rowDetail.appendChild(divInfo);
-        divInfo.appendChild(rowTitle);
-        rowTitle.appendChild(col);
-        col.appendChild(cardName);
-        cardName.appendChild(pDescription);
-
-        divInfo.appendChild(aFocus);
-        aFocus.appendChild(rowPrice);
-        rowPrice.appendChild(label);
-        label.appendChild(input);
-        rowPrice.appendChild(divRowFocus);
-
-        detailCard.appendChild(rowLower);
-        rowLower.appendChild(divButton);
-        divButton.appendChild(buttonAddCart);
-
+    if (!panier) {
+        panier = {
+            cameras: [camera],
+            total: camera.price
+        }
+        localStorage.setItem('panier', JSON.stringify(panier));
+    }
+    else {
+        let panierObject = JSON.parse(panier);
+        panierObject.cameras.push(camera);
+        panierObject.total = Number(panierObject.total) + Number(camera.price);
+        localStorage.setItem('panier', JSON.stringify(panierObject));
     }
 }
